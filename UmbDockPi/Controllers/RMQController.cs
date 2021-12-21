4a0d1f8aac5cae6a57e2dde24b8eb65e78525d6a
Carl Sargunar <carl@sargunar.com>
2021-12-21 18:05:53 +0000

    Controller Update

--- UmbDockPi/Controllers/RMQController.cs
+++ UmbDockPi/Controllers/RMQController.cs
@@ -13,29 +13,36 @@ namespace UmbDockPi.Controllers
 
         public Microsoft.AspNetCore.Mvc.RedirectResult SendMessage(string name)
         {
-            // TODO : Put the hostname in Config
-            // var factory = new ConnectionFactory() { HostName = "192.168.0.144" };
-            var factory = new ConnectionFactory() { HostName = "localhost" };
-            using (var connection = factory.CreateConnection())
-            using (var channel = connection.CreateModel())
+            try
             {
-                channel.QueueDeclare(queue: "hello",
-                                     durable: false,
-                                     exclusive: false,
-                                     autoDelete: false,
-                                     arguments: null);
+                // TODO : Put the hostname in Config
+                // var factory = new ConnectionFactory() { HostName = "192.168.0.144" };
+                var factory = new ConnectionFactory() { HostName = "localhost" };
+                using (var connection = factory.CreateConnection())
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare(queue: "hello",
+                                         durable: false,
+                                         exclusive: false,
+                                         autoDelete: false,
+                                         arguments: null);
 
-                string message = $"Hey {name}! Happy 24 Days of Umbraco. #h5yr!";
-                var body = Encoding.UTF8.GetBytes(message);
+                    string message = $"Hey {name}! Happy 24 Days of Umbraco. #h5yr!";
+                    var body = Encoding.UTF8.GetBytes(message);
 
-                channel.BasicPublish(exchange: "",
-                                     routingKey: "hello",
-                                     basicProperties: null,
-                                     body: body);
+                    channel.BasicPublish(exchange: "",
+                                         routingKey: "hello",
+                                         basicProperties: null,
+                                         body: body);
+                }
+            }
+            catch
+            {
+                
             }
 
             // Being Lazy :-)
-            return Redirect("/");
+            return Redirect("/thank-you/");
         }
 
     }
